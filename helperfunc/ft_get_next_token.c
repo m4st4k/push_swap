920b79b143c4e45001de703efba6ff002c43941d
m4st4k <desulmehgh@gmail.com>
2025-06-16 03:27:32 +0200

    update

--- helperfunc/ft_get_next_token.c
+++ helperfunc/ft_get_next_token.c
@@ -19,22 +19,24 @@ static int	is_white_space(char c)
 
 char	*get_next_token(char *str)
 {
-	static char	*str_current;
 	char		*token;
+	static char	*str_current;
 
 	if (str != NULL)
 		str_current = str;
-	while (*str_current != '\0' && is_white_space(*str_current))
-		str_current++;
-	if (*str_current == '\0')
+	while (is_white_space(*str_current))
+		++str_current;
+	if (character == '\0')
 		return (NULL);
 	token = str_current;
-	while (*str_current != '\0' && !is_white_space(*str_current))
-		str_current++;
-	if (is_white_space(*str_current))
+	while (*str_current != '\0')
 	{
-		*str_current = '\0';
-		str_current++;
+		if (is_white_space(character))
+		{
+			*(str_current++) = '\0';
+			break ;
+		}
+		++str_current;
 	}
 	return (token);
 }
